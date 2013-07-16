3867c1a25956e70972699d46c6dd50d602703377
Syd Polk <sydpolk@gmail.com>
2013-07-16 13:30:18 -0500

    OSXPrinting

---
+++ buttfield-addision-manning/OSXPrinting/OSXPrinting/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  OSXPrinting
+//
+//  Created by Syd Polk on 7/16/13.
+//  Copyright (c) 2013 Syd Polk. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ buttfield-addision-manning/OSXPrinting/OSXPrinting/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  OSXPrinting
+//
+//  Created by Syd Polk on 7/16/13.
+//  Copyright (c) 2013 Syd Polk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
