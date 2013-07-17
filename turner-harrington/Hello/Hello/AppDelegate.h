e8af2e6017db34cc2765a050e4bb5d67836d708d
Syd Polk <sydpolk@gmail.com>
2013-07-17 17:21:33 -0500

    Hello, World

---
+++ turner-harrington/Hello/Hello/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by Syd Polk on 7/17/13.
+//  Copyright (c) 2013 Syd Polk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
