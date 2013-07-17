e0fdf98ed1e549359b9f733e7a240dbeded06004
Syd Polk <sydpolk@gmail.com>
2013-07-17 14:06:34 -0500

    Localized

---
+++ buttfield-addision-manning/Localized/Localized/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Localized
+//
+//  Created by Syd Polk on 7/17/13.
+//  Copyright (c) 2013 Syd Polk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
