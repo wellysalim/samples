55b354627d36053bdca7640df2195f61ee91e786
Syd Polk <syd@rocksys.com>
2013-07-17 22:00:16 -0500

    As created by Xcode.

---
+++ turner-harrington/PhotoWheelPrototype/PhotoWheelPrototype/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  PhotoWheelPrototype
+//
+//  Created by Syd Polk on 07/17/13.
+//  Copyright (c) 2013 Syd Polk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
