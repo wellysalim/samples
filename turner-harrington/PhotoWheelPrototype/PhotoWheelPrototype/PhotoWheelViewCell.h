07ed468aec49d1f677012ca99192f73cb756adad
Syd Polk <sydpolk@gmail.com>
2013-07-18 14:33:01 -0500

    Use real images.

---
+++ turner-harrington/PhotoWheelPrototype/PhotoWheelPrototype/PhotoWheelViewCell.h
@@ -0,0 +1,15 @@
+//
+//  PhotoWheelViewCell.h
+//  PhotoWheelPrototype
+//
+//  Created by Syd Polk on 7/18/13.
+//  Copyright (c) 2013 Syd Polk. All rights reserved.
+//
+
+#import "WheelView.h"
+
+@interface PhotoWheelViewCell : WheelViewCell
+
+- (void) setImage:(UIImage *) newImage;
+
+@end
