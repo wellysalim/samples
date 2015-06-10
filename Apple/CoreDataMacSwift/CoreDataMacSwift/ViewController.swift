dc7d348aed661e69ee1fd144603fe79785a97fed
Syd Polk <sydpolk@gmail.com>
2015-06-10 08:54:17 -0700

    Added CoreData project from Mac.

---
+++ Apple/CoreDataMacSwift/CoreDataMacSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoreDataMacSwift
+//
+//  Created by Syd Polk on 6/9/15.
+//  Copyright © 2015 Bone Jarring Games and Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
