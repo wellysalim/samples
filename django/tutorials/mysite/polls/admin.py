f674fe1d8f8b2b55e8fa4654d38a6316a3471b73
Syd Polk <syd@indeed.com>
2016-06-30 16:09:02 -0500

    django/tutorials/mysite - Customize admin pages. Finished tutorial.

--- django/tutorials/mysite/polls/admin.py
+++ django/tutorials/mysite/polls/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
